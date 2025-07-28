8b4ca136c02c3243d3839173e36cb3e08f396a2d
Matt Brown <matt@onevision.co.uk>
2025-07-28 18:01:06 +0100

    fix: transform .mjs modules and correct transformIgnorePatterns for mongodb-memory-server

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,18 @@
 /** @type {import('ts-jest').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  // Ignore Next.js build output
+  testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/.next/'],
-  // File extensions Jest will process
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'mjs'],
-  // Setup file for extending Jest
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  // Transform TS/JS files via ts-jest
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    // Transform TS/JS and also ESM .mjs via ts-jest
+    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.mjs$': 'ts-jest'
   },
-  // Allow ts-jest to transform these ESM modules
+  // Do _not_ ignore these ESM modules
   transformIgnorePatterns: [
-    '/node_modules/(?!(bson|mongodb|mongodb-memory-server)/)'
+    'node_modules/(?!(bson|mongodb|mongodb-memory-server)/)'
   ],
   globals: {
     'ts-jest': {
